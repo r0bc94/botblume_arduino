63af405f336efddfac474bad97abdb3aeae39988
Robin Palkovits <robin.pal@hotmail.de>
2019-11-29 00:20:12 +0100

    :alembic: Add some Test Code for the Network Manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
 
+#include "network_manager/network_manager.h"
+#include "types/message.h"
+
+const char* ssid     = "~PrettyFlyForAWiFi~";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "TN8Y=UL8Dz84n6v#";     // The password of the Wi-Fi network
+
+const char *mqttIp = "192.168.1.161";
+const int mqttPort = 1883;
+
+NetworkManager netman;
+
 void setup() {
+  Serial.begin(9600);
+  netman = NetworkManager();
+  
+  Serial.println("Connecting to wifi");
+  netman.connectWiFi(ssid, password);
+  netman.connectMqtt(mqttIp, mqttPort);
+
   // put your setup code here, to run once:
 }
 
 void loop() {
+  struct Message msg;
+  msg.originalValue = 100;
+  msg.percentage = 50;
+
+  Serial.println("Sending Test Message");
+  Serial.println(netman.sendMessage(&msg, "test"));
+
+  delay(5000);
   // put your main code here, to run repeatedly:
 }
